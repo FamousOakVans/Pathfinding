630e239a009d9eb746330f051e03eb0d0a54954f
Ivan <drillsmail@gmail.com>
2018-02-12 23:07:24 +0300

    Выравнивание текста внутри ноды и начало работы над перетаскиванием View

--- app/src/main/java/com/janeandjohnstudio/pathfinding/graphics/Node.kt
+++ app/src/main/java/com/janeandjohnstudio/pathfinding/graphics/Node.kt
@@ -5,19 +5,32 @@ import android.content.res.TypedArray
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
+import android.support.v4.view.GestureDetectorCompat
 import android.text.TextPaint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import com.janeandjohnstudio.pathfinding.R
+import android.support.v4.view.MotionEventCompat
+import android.view.MotionEvent.INVALID_POINTER_ID
+import android.text.method.Touch.onTouchEvent
+import android.view.DragEvent
+
 
 class Node : View {
 
     private lateinit var value : String
 //    private var valueSize : Float
 
-    private var backgroundPaint: Paint = Paint()
-    private var textPaint: TextPaint = TextPaint(Paint.ANTI_ALIAS_FLAG)
-    private var borderPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val backgroundPaint: Paint = Paint()
+    private val textPaint: TextPaint = TextPaint(Paint.ANTI_ALIAS_FLAG)
+    private val borderPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val textRect : Rect = Rect()
+
+    private var lastTouchX = 0F
+    private var lastTouchY = 0F
+    private var activePointerId = 0
 
     init {
         borderPaint.style = Paint.Style.STROKE
@@ -58,7 +71,7 @@ class Node : View {
 
     private fun setNodeValue(a: TypedArray, attrs: AttributeSet?) {
 
-        this.value = a.getString(R.styleable.Node_nodeValue)
+        this.value = a.getString(R.styleable.Node_nodeName)
 
         val set = intArrayOf(android.R.attr.textSize)
 
@@ -94,8 +107,85 @@ class Node : View {
         canvas.drawCircle(xCenter, yCenter, radius, backgroundPaint)
         canvas.drawCircle(xCenter, yCenter, radius, borderPaint)
 
-        canvas.drawText(value, xCenter, yCenter, textPaint)
+//        val textWidth = textPaint.measureText(value)
+        textPaint.getTextBounds(value, 0, value.length, textRect)
+        textPaint.fontMetrics.ascent
+        canvas.drawText(value, xCenter - textRect.width() / 2, yCenter + textRect.height() / 2 - textPaint.descent(), textPaint)
 
-        canvas.translate(0F, 200F)
+//        canvas.translate(0F, 200F)
     }
+
+    override fun onDragEvent(event: DragEvent?): Boolean {
+        return super.onDragEvent(event)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+//        dete.onTouchEvent(ev)
+
+        val action = event?.actionMasked
+
+        when (action) {
+            MotionEvent.ACTION_DOWN -> {
+                val x = event.getX(event.actionIndex)
+                val y = event.getY(event.actionIndex)
+
+                // Remember where we started (for dragging)
+                lastTouchX = x
+                lastTouchY = y
+                // Save the ID of this pointer (for dragging)
+                activePointerId = event.getPointerId(0)
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                // Find the index of the active pointer and fetch its position
+                val pointerIndex = event.findPointerIndex(activePointerId)
+
+                val x = event.getX(pointerIndex)
+                val y = event.getY(pointerIndex)
+
+                // Calculate the distance moved
+                val dx = x - lastTouchX
+                val dy = y - lastTouchY
+
+//                mPosX += dx
+//                mPosY += dy
+
+                invalidate()
+
+                // Remember this touch position for the next move event
+                lastTouchX = x
+                lastTouchY = y
+            }
+
+            MotionEvent.ACTION_UP -> {
+                activePointerId = INVALID_POINTER_ID
+            }
+
+            MotionEvent.ACTION_CANCEL -> {
+                activePointerId = INVALID_POINTER_ID
+            }
+
+            MotionEvent.ACTION_POINTER_UP -> {
+
+                val pointerIndex = event.actionIndex
+                val pointerId = event.getPointerId(pointerIndex)
+
+                if (pointerId == activePointerId) {
+                    // This was our active pointer going up. Choose a new
+                    // active pointer and adjust accordingly.
+                    val newPointerIndex = if (pointerIndex == 0) 1 else 0
+                    lastTouchX = event.getX(newPointerIndex)
+                    lastTouchY = event.getY(newPointerIndex)
+                    activePointerId = event.getPointerId(newPointerIndex)
+                }
+            }
+        }
+        return true
+    }
+
+//    override fun performClick(): Boolean {
+//        super.performClick()
+//        return true
+//    }
 }
